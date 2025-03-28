207a6e9d73e3f2b91695ed36ae68eaffbd870322
mikiyasmechalo <mikiyasmechalo70@gmail.com>
2025-03-28 21:30:06 +0300

    No ESLint warnings or errors

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,4 @@
 import SideNav from "@/app/ui/dashboard/sidenav";
-import { Metadata } from 'next';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -55,7 +55,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
 `;
-  } catch (_) {
+  } catch {
     return {
       message: "Database Error: Failed to Create Invoice.",
     };
@@ -94,7 +94,7 @@ export async function updateInvoice(
         SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
         WHERE id = ${id}
       `;
-  } catch (_) {
+  } catch {
     return { message: "Database Error: Failed to Update Invoice." };
   }
 
